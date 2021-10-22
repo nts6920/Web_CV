5b64df88c678f99289705cd8b2bd37dcca3c15ac
nts6920 <nts6920@gmail.com>
2021-10-22 17:28:27 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include('./include/db.php'); 
+$query = "SELECT * FROM basic_setup,personal_setup,aboutus_setup";
+$runquery = mysqli_query($db,$query);
+if(!$db){
+    header("location:index_db.html");
+}
+$data = mysqli_fetch_array($runquery);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
